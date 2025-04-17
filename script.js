da9a7099ca3690f22b47715bb9562a73ba7a49ca
Christopher <chrisric0725@gmail.com>
2025-04-16 21:24:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ let display = document.getElementById("display");
 function adicionarValor(valor) {
     display.value += valor;
 }
-    function buttonClicked(buttonNumber){
-        alert('Voce clicou no botao ' + buttonNumber);
-    }
+    
     function adicionarValor(valor) {
     display.value += valor;
 }
