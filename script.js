78d7f2b93aa4723cc1d1470f3dce57f3f0d856ed
Christopher <chrisric0725@gmail.com>
2025-04-16 21:25:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let display = document.getElementById("display");
-function adicionarValor(valor) {
-    display.value += valor;
-}
+
     
     function adicionarValor(valor) {
     display.value += valor;
