828ec9683ea29f5412b62dd97eec06d75390bc45
Christopher <chrisric0725@gmail.com>
2025-04-16 21:22:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,13 @@ function adicionarValor(valor) {
     function buttonClicked(buttonNumber){
         alert('Voce clicou no botao ' + buttonNumber);
     }
-    
+    function adicionarValor(valor) {
+    display.value += valor;
+}
+function calcular() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Erro";
+    }
+}
